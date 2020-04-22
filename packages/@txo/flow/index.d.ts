581145396ab969e84b090f0ba747eb1a8e0feebd
Ondrej Mikuláš <ondrej.mikulas@technologystudio.sk>
2020-04-22 21:05:28 +0200

    Add index.d.ts

--- packages/@txo/flow/index.d.ts
+++ packages/@txo/flow/index.d.ts
@@ -1,2 +1,8 @@
 declare module "@txo/flow" {
+  type LiteralMap<KEY, VALUE> = {
+    [KEY]: VALUE,
+  }
+  const is = <TYPE> (value: ?TYPE): TYPE
+  const extend = <TYPE_EXTENSION, TYPE: Object>(value: TYPE): TYPE & TYPE_EXTENSION
+  const varianceFix = <TYPE: Object>(value: TYPE)
 }
